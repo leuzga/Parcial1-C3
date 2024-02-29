cc6528c6ad63a7eaf7399c64bcaa4924fc2fd5cc
Lenin Uzcategui <c-luzcategu@cdsreg.com>
2024-02-28 23:08:56 -0300

    Parcial 1 Camada 3 Lenin Uzcategui

--- parcial1-C3/src/Components/Cards/Card.jsx
+++ parcial1-C3/src/Components/Cards/Card.jsx
@@ -1,12 +1,17 @@
 import './Card.css'; // Asegúrate de tener un archivo CSS para estilizar tu componente
 
-const Card = ({ imageUrl, title, description }) => {
+const Card = ({ imageUrl, title, description, formData }) => {
   return (
     <div className='card'>
       <img src={imageUrl} alt={title} className='card-image' />
       <div className='card-content'>
         <h2 className='card-title'>{title}</h2>
         <p className='card-description'>{description}</p>
+        <p>Nombre: {formData.nombre}</p>
+        <p>Edad: {formData.edad}</p>
+        <p>Cantidad de Personas: {formData.cantidadPersonas}</p>
+        <p>Lugar: {formData.lugar}</p>
+        <p>Monto Total: {formData.montoTotal}</p>
       </div>
     </div>
   );
